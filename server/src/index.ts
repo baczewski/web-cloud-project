3a7776550a6a3146dd2af1c009fcef9a85887fa1
Kristiyan Voynski <37977687+404carrynotfound@users.noreply.github.com>
2023-06-15 20:16:00 +0300

    db init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,39 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import {UserEntity} from "../../../web-in-the-clouds/server/src/entity/UserEntity";
+import {NoteEntity} from "../../../web-in-the-clouds/server/src/entity/NoteEntity";
+import {AppDataSource} from "./data-source";
 
 dotenv.config();
+
+AppDataSource.initialize().then(async () => {
+
+    console.log("Inserting a new user into the database...")
+    const user = new UserEntity()
+    user.firstName = "Timber"
+    user.lastName = "Saw"
+    user.email = "test@test.test"
+    user.password = "hashedPass"
+    await AppDataSource.manager.save(user)
+    console.log("Saved a new user with id: " + user.id)
+
+    console.log("Loading users from the database...")
+    const users = await AppDataSource.manager.find(UserEntity)
+    console.log("Loaded users: ", users)
+
+    console.log("Inserting a new note into the database...")
+    const note = new NoteEntity()
+    note.user = user;
+    note.title = "Test note"
+    note.description = "Test content :)"
+    await AppDataSource.manager.save(note);
+    console.log("Saved a new note with id: " + note.id)
+
+    console.log("Loading notes from the database...")
+    const notes = await AppDataSource.manager.find(NoteEntity)
+    console.log("Loaded notes: ", notes)
+
+}).catch(error => console.log(error))
 
 const app = express();
 app.use(express.json());
