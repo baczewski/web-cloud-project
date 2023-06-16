3d0d854716189ebc1b8e7aeb4f1669aa88a0ec68
Odeliks <odeliks12@gmail.com>
2023-06-16 20:53:26 +0300

    Add login and register route

--- client/src/components/InputField/InputField.tsx
+++ client/src/components/InputField/InputField.tsx
@@ -10,11 +10,13 @@ export const InputField = ({
 ) => {
   return (
     <Input
+      style={{color: "white", backgroundColor: "#525252", paddingLeft: "0.5rem", padding: "0.7rem"}}
       placeholder={placeholder}
       onChange={onChange}
       type={type}
       value={value}
       required
+      fullWidth
     />
   );
 };
--- client/src/pages/register/RegisterPage.tsx
+++ client/src/pages/register/RegisterPage.tsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import { InputField } from "../../components/InputField/InputField";
 import { Button } from "@mui/material";
-import { login } from "./loginUtils";
 import { Link } from "react-router-dom";
-import "./LoginPage.css";
+import "./RegisterPage.css";
 
-export const LoginPage = () => {
+
+export const RegisterPage = () => {
 
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("")
 
   return (
     <div className="container">
-      <form className="login-container">
+      <form className="register-container">
         <InputField
           onChange={(event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => setName(event.target.value)}
           placeholder="Username"
@@ -25,18 +26,24 @@ export const LoginPage = () => {
           type="password"
           value={password}
         />
+        <InputField
+          onChange={(event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => setEmail(event.target.value)}
+          placeholder="E-mail"
+          type="text"
+          value={email}
+        />
         <Button
           variant="contained"
           style={{ backgroundColor: "white", color: "black", width: "50%", alignSelf: "center" }}
           onClick={() => {
-            login(name, password);
+            console.log(name, password);
             setName("");
             setPassword("");
           }}>
-          Login
+          Register
         </Button>
-        <span className="register">
-          Don't have an account? <Link className="register-link" to="/register">Register here</Link>
+        <span className="login">
+          Already have an account? <Link className="login-link" to="/login">Log in here</Link>
         </span>
       </form>
     </div>
