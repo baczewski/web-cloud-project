f25003347e42973a57040d97cf22372cfd38196f
Kristiyan Voynski <37977687+404carrynotfound@users.noreply.github.com>
2023-06-18 22:55:42 +0300

    Fix layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LoginPage } from "./pages/login/LoginPage";
 import { RegisterPage } from "./pages/register/RegisterPage";
 import HomePage from "./pages/home/HomePage";
-import Layout from './components/Layout';
+import Layout from './components/Layout/Layout';
 import { useStyles } from './AppStyles';
+import Details from './components/Details/Details';
+import { Type } from './utils/Type';
 
 const App = () => {
   const classes = useStyles();
@@ -16,6 +18,7 @@ const App = () => {
           <Route path="/register" element={<RegisterPage />} />
           <Route element={<Layout />}>
             <Route path="/home" element={<HomePage />} />
+            <Route path="/details" element={<Details title='Title' type={Type.NOTE} />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,14 +1,8 @@
 import { Note } from '../../components/Note/Note';
-import { useStyles } from './HomePageStyles';
 
 const HomePage = () => {
-    const classes = useStyles();
-
     return (
-        <main className={classes.content}>
-            <div className={classes.toolbar} />
             <Note />
-        </main>
     );
 };
 
