943a2f8cea0ec8c884c3e8b28c44dbb6418dc125
Odeliks <odeliks12@gmail.com>
2023-06-19 13:17:54 +0300

    Add create todo modal and button

--- client/src/pages/todo/AssignmentPage.tsx
+++ client/src/pages/todo/AssignmentPage.tsx
@@ -2,13 +2,16 @@ import { Box, List } from "@material-ui/core";
 import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../../context/AppContext";
 import { Todo, TodoInterface } from "../../components/Todo/Todo";
+import FloatingButton from "../../components/FloatingButton/FloatingButton";
+import AddIcon from '@mui/icons-material/Add';
+import CreateModal from "../../components/CreateModal/CreateModal";
+import { FetchType } from "../../components/CreateModal/types";
 
 export const AssignmentPage = () => {
-  const { globalState } = useContext(GlobalContext);
+  const fake = [{ id: "asd", title: "1", description: "asadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssd", dueDate: new Date() }, { id: "asd", title: "12", description: "asd", dueDate: new Date() }]
 
-  const fake = [{id: "asd", title:"1", description:"asadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssdasadsssssssssssssssssssssssd", dueDate: new Date()},{id: "asd", title:"12", description:"asd", dueDate: new Date()}]
-
-  const [todos, setTodos] = useState<TodoInterface[]>([])
+  const [todos, setTodos] = useState<TodoInterface[]>(fake);
+  const [showModal, setShowModal] = useState(false);
 
   // useEffect(() => {
   //   fetch("http://localhost:8080/todos", {
@@ -25,8 +28,15 @@ export const AssignmentPage = () => {
     <Box>
       <h2>TODO List</h2>
       <List>
-        {todos.map((todo) => <Todo {...todo}/>)}
+        {todos.map((todo) => <Todo {...todo} />)}
       </List>
+      <CreateModal
+        open={showModal}
+        onClose={() => setShowModal(false)}
+        hasDateInput
+        type={FetchType.todos}
+      />
+      <FloatingButton text='Add Todo' icon={<AddIcon />} changeEvent={setShowModal} />
     </Box>
   );
 };
